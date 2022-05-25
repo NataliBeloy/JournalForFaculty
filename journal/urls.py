ca581fa65fcd021f972e804dcc178984dbe2f7c2
NataliBeloy <natashabelousova555@gmail.com>
2022-05-25 15:30:28 +0200

    report_add

--- journal/urls.py
+++ journal/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
+from . import views
 
-from .views import GroupStudentListView, GroupStudentDetailView, ScoreLessonListView, AddScore
+from .views import GroupStudentListView, GroupStudentDetailView, ScoreLessonListView, AddScore, CreateReport
 
 
 urlpatterns = [
@@ -9,4 +10,5 @@ urlpatterns = [
     path('group/<int:pk>/', GroupStudentDetailView.as_view(), name='group_student_detail'),
     path('<int:group_id>/<int:lesson_id>/', ScoreLessonListView.as_view(), name='score_lesson'),
     path('addscore/', AddScore.as_view(), name='add_score'),
+    path('generate-pdf/', CreateReport.as_view(), name='generate-pdf')
 ]
--- journal/views.py
+++ journal/views.py
@@ -1,14 +1,64 @@
+from datetime import datetime
+from io import BytesIO
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import View
+from reportlab.lib.pagesizes import A4
+from reportlab.pdfgen import canvas
 
 from django_journal.permissions import TeacherPermissionsMixin, TeacherLessonPermissionsMixin
 from django_journal.settings import TEACHER
 from people.models import User
-from .models import GroupStudent, Score, Lesson
 from utils.service import ScoreJournalMixin
+from .models import GroupStudent, Score, Lesson
+
+
+class CreateReport(LoginRequiredMixin, View):
+
+    def get(self, request):
+        response = HttpResponse(content_type='application/pdf')
+        d = datetime.today().strftime('%Y-%m-%d')
+        response['Content-Disposition'] = f'inline; filename="{d}.pdf"'
+
+        buffer = BytesIO()
+        p = canvas.Canvas(buffer, pagesize=A4)
+
+        # Data to print
+        data = {
+            "Posts": [{"title": "Python", "views": 500}, {"title": "JavaScript", "views": 500}],
+            "Videos": [{"title": "Python Programming", "likes": 500}],
+            "Blogs": [{"name": "Report Lab", "likes": 500, "claps": 500}],
+        }
+
+        p.setFont("Helvetica", 14, leading=None)
+        p.setFillColorRGB(0.29296875, 0.453125, 0.609375)
+        p.drawString(260, 800, "My PDF")
+        p.line(0, 780, 1000, 780)
+        p.line(0, 778, 1000, 778)
+        xl = 20
+        yl = 750
+
+        for k, v in data.items():
+            p.setFont("Helvetica", 14, leading=None)
+            p.drawString(xl, yl - 12, f"{k}")
+            for value in v:
+                for key, val in value.items():
+                    p.setFont("Helvetica", 12, leading=None)
+                    p.drawString(xl, yl - 20, f"{key} - {val}")
+                    yl = yl - 60
+        p.setTitle(f'Report on {d}')
+        p.showPage()
+        p.save()
+
+        pdf = buffer.getvalue()
+        buffer.close()
+        response.write(pdf)
+
+        return response
 
 
 class GroupStudentListView(LoginRequiredMixin, TeacherPermissionsMixin, ListView):
@@ -29,7 +79,7 @@ class GroupStudentDetailView(LoginRequiredMixin, TeacherPermissionsMixin, Detail
 
     def get_context_data(self, **kwargs):
         context = super(GroupStudentDetailView, self).get_context_data()
-        context['students'] = User.objects.select_related('student__group')\
+        context['students'] = User.objects.select_related('student__group') \
             .filter(student__group_id=self.kwargs['pk'])
         return context
 
@@ -44,7 +94,7 @@ class ScoreLessonListView(LoginRequiredMixin, TeacherLessonPermissionsMixin, Sco
         group_student = GroupStudent.objects.select_related('grade')
         self.group = get_object_or_404(group_student, id=self.kwargs['group_id'])
         self.lesson = get_object_or_404(Lesson, id=self.kwargs['lesson_id'])
-        queryset = Score.objects.select_related('group', 'lesson')\
+        queryset = Score.objects.select_related('group', 'lesson') \
             .filter(group_id=self.kwargs['group_id'], lesson_id=self.kwargs['lesson_id'])
         return queryset
 
@@ -53,7 +103,7 @@ class ScoreLessonListView(LoginRequiredMixin, TeacherLessonPermissionsMixin, Sco
         date_period = self.create_date_period_list()
         students = User.objects.select_related('student', 'student__group').filter(student__group=self.group)
 
-        scores = Score.objects.select_related('group', 'lesson')\
+        scores = Score.objects.select_related('group', 'lesson') \
             .filter(created__in=date_period, lesson_id=self.lesson, group_id=self.group)
 
         context['date_period'] = date_period
@@ -68,6 +118,7 @@ class ScoreLessonListView(LoginRequiredMixin, TeacherLessonPermissionsMixin, Sco
 
 class AddScore(LoginRequiredMixin, TeacherLessonPermissionsMixin, View):
     """Додавання оцінки."""
+
     def post(self, request):
         score_id = request.POST.get('score_id')
         if score_id == '0':
